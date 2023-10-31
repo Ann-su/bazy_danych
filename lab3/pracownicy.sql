6c0f9f2b29378722d13ac5c67cbce80802bd250d
Anna Suska <anna.suska@icloud.com>
2023-10-31 19:29:17 +0100

    Add files via upload

---
+++ lab3/pracownicy.sql
@@ -0,0 +1,12 @@
+INSERT INTO rozliczenia.pracownicy (id_pracownika, imie, nazwisko, adres, telefon)
+VALUES
+    (1, 'Jan', 'Kowalski', 'ul. Prosta 123', '123-456-789'),
+    (2, 'Anna', 'Nowak', 'ul. Kwiatowa 456', '987-654-321'),
+    (3, 'Marek', 'Wiśniewski', 'ul. Główna 789', '555-123-789'),
+    (4, 'Alicja', 'Kaczmarek', 'ul. Słoneczna 42', '111-222-333'),
+    (5, 'Piotr', 'Lis', 'ul. Brzozowa 15', '444-777-888'),
+    (6, 'Ewa', 'Zielińska', 'ul. Ogrodowa 67', '999-888-777'),
+    (7, 'Tomasz', 'Wójcik', 'ul. Polna 33', '222-333-444'),
+    (8, 'Magda', 'Pawłowska', 'ul. Jasna 89', '888-111-222'),
+    (9, 'Krzysztof', 'Jankowski', 'ul. Długa 12', '444-555-666'),
+    (10, 'Karolina', 'Nowicka', 'ul. Parkowa 54', '777-999-555');
---
+++ lab3/premie.sql
@@ -0,0 +1,12 @@
+INSERT INTO rozliczenia.premie (id_premii, rodzaj, kwota)
+VALUES
+    (1, 'Rodzaj1', 1000),
+    (2, 'Rodzaj2', 1500),
+    (3, 'Rodzaj3', 2000),
+    (4, 'Rodzaj4', 1200),
+    (5, 'Rodzaj5', 1800),
+    (6, 'Rodzaj6', 1350),
+    (7, 'Rodzaj7', 1650),
+    (8, 'Rodzaj8', 2100),
+    (9, 'Rodzaj9', 1400),
+    (10, 'Rodzaj10', 1900);
